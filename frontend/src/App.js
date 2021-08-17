4f026e799dec9d30b301c8227b4f2f97cdbf6fc0
maxyee <eyaminkhan00@gmail.com>
2021-08-17 20:55:05 +0600

    added another page to the project called addbike

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import Header from "./components/Header";
 import Bike from "./components/Bike";
 import Footer from "./components/Footer";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddBike from "./pages/AddBike";
+import AllBike from "./pages/AllBike";
 
 function App() {
   return (
     <div className="font-mono flex flex-col h-screen justify-between">
       <Router>
         <Switch>
-        <Route exact path="/addbike">
+          <Route exact path="/allbike">
             <Header />
-              <AddBike />
+            <AllBike />
+            <Footer />
+          </Route>
+          <Route exact path="/addbike">
+            <Header />
+            <AddBike />
             <Footer />
           </Route>
           <Route exact path="/">
             <Header />
-              <Bike />
-              <Bike />
-              <Bike />
-              <Bike />
+            <Bike />
+            <Bike />
+            <Bike />
+            <Bike />
             <Footer />
           </Route>
         </Switch>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,16 +1,21 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <div>
-            <div className="flex justify-center text-4xl bg-gray-400">
-                Sports Bike Showroom
-            </div>
-            <Link to="/addbike">
-                <button className="bg-gray-200">AddBike</button>
-            </Link> 
-        </div>
-        
-    )
+  return (
+    <div>
+      <div className="flex justify-center text-4xl bg-gray-400">
+        Sports Bike Showroom
+      </div>
+      <Link to="/">
+        <button className="bg-gray-200">Home</button>
+      </Link>
+      <Link to="/addbike">
+        <button className="bg-gray-200 mx-6">AddBike</button>
+      </Link>
+      <Link to="/allbike">
+        <button className="bg-gray-200">AllBike</button>
+      </Link>
+    </div>
+  );
 }
